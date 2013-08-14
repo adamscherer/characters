e556d7130a85af82a5431ed0a94301a56a1dc8ea
Adam Scherer <adam@annconia.com>
2013-08-13 21:31:36 -0500

    First pages commit

--- js/application.js
+++ js/application.js
@@ -1,67 +1,28 @@
-// Some general UI pack related JS
-// Extend JS String with repeat method
-String.prototype.repeat = function(num) {
-    return new Array(num + 1).join(this);
-};
-
 (function($) {
-
-  // Add segments to a slider
-  $.fn.addSliderSegments = function (amount) {
-    return this.each(function () {
-      var segmentGap = 100 / (amount - 1) + "%"
-        , segment = "<div class='ui-slider-segment' style='margin-left: " + segmentGap + ";'></div>";
-      $(this).prepend(segment.repeat(amount - 2));
-    });
-  };
 
   $(function() {
   
-    // Todo list
-    $(".todo li").click(function() {
-        $(this).toggleClass("todo-done");
-    });
-
-    // Custom Select
-    $("select[name='herolist']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
-
-    // Tooltips
-    $("[data-toggle=tooltip]").tooltip("show");
-
-    // Tags Input
-    $(".tagsinput").tagsInput();
-
-    // jQuery UI Sliders
-    var $slider = $("#slider");
-    if ($slider.length) {
-      $slider.slider({
-        min: 1,
-        max: 5,
-        value: 2,
-        orientation: "horizontal",
-        range: "min"
-      }).addSliderSegments($slider.slider("option").max);
-    }
+    var input = $('#data-input');
+    var result = $('#data-result');
+    var resultElement = result.find('h3');
 
     // Placeholders for input/textarea
     $("input, textarea").placeholder();
 
-    // Make pagination demo work
-    $(".pagination a").on('click', function() {
-      $(this).parent().siblings("li").removeClass("active").end().addClass("active");
-    });
-
-    $(".btn-group a").on('click', function() {
-      $(this).siblings().removeClass("active").end().addClass("active");
-    });
-
     // Disable link clicks to prevent page scrolling
-    $('a[href="#fakelink"]').on('click', function (e) {
+    $('a[href="#count"]').on('click', function (e) {
       e.preventDefault();
+      resultElement.text($("textarea").val().length);
+      input.hide();
+      result.show();
     });
 
-    // Switch
-    $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
+    $('a.login-link').on('click', function (e) {
+      e.preventDefault();
+      input.show();
+      result.hide();
+    });
+
     
   });
   
