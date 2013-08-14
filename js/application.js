d94de28132bc2bee79946da084366b10d33d0b27
adamscherer <adam@annconia.com>
2013-08-14 08:37:24 -0500

    updates

--- js/application.js
+++ js/application.js
@@ -6,13 +6,17 @@
     var result = $('#data-result');
     var resultElement = result.find('h3');
 
+    var numberWithCommas : function(x) {
+      return (x || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
     // Placeholders for input/textarea
     $("input, textarea").placeholder();
 
     // Disable link clicks to prevent page scrolling
     $('a[href="#count"]').on('click', function (e) {
       e.preventDefault();
-      resultElement.text($("textarea").val().length);
+      resultElement.text(numberWithCommas($("textarea").val().length));
       input.hide();
       result.show();
     });
